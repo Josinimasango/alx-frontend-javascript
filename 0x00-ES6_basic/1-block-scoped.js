895bf99da78a91a4b7fac78d1b4d0d01e88e080a
Josinimasango <nellyjossydumako@gmail.com>
2024-05-27 18:31:58 +0000

    block scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,10 +3,10 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-   // Modifying the outer task variable	  
-   const  task = true;
-   // Modifying the outer task2 variable	  
-   const  task2 = false;
+    // eslint-disable-next-line
+    const task = true;
+    // eslint-disable-next-line
+    const task2 = false;
   }
 
   return [task, task2];
