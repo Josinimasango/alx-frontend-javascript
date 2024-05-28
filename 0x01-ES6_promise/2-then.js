beb6a880f0e7affb1c0d4a13cfa31bbd8719cd4d
Josinimasango <nellyjossydumako@gmail.com>
2024-05-28 19:45:38 +0000

    then

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,16 @@
+export default function handleResponseFromAPI(promise) {
+    return promise
+        .then((result) => {
+            console.log('Got a response from the API');
+            return {
+                status: 200,
+                body: 'success'
+            };
+        })
+        .catch((error) => {
+            return new Error();
+        })
+        .finally(() => {
+            console.log('Got a response from the API');
+        });
+}
