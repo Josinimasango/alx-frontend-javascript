66abbfdd3b15e651353d60cea0c92bc5997eac3a
root <nellyjossydumako@gmail.com>
2024-06-05 21:36:05 +0000

    Printing teachers

--- 0x04-TypeScript/task_1/js/main.js
+++ 0x04-TypeScript/task_1/js/main.js
@@ -6,3 +6,8 @@ var director1 = {
     numberOfReports: 17,
 };
 console.log(director1);
+var printTeacher = function (firstName, lastName) {
+    var firstInitial = firstName.charAt(0);
+    return "".concat(firstInitial, ". ").concat(lastName);
+};
+console.log(printTeacher("John", "Doe"));
--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -18,3 +18,14 @@ const director1: Directors = {
 };
 
 console.log(director1);
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  const firstInitial = firstName.charAt(0);
+  return `${firstInitial}. ${lastName}`;
+};
+
+console.log(printTeacher("John", "Doe"));
